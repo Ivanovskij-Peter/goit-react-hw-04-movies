034a21903dc5f3fbd12a2ccaf52860ee0654f8cc
Peter Ivanovskij <65976810+Ivanovskij-Peter@users.noreply.github.com>
2020-09-14 17:20:17 +0300

    Updates

---
+++ precache-manifest.89ecfcebafcddd99ebd206900db98bb8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8734c60fde79cd118c07c6b6a3a64e6e",
+    "url": "/goit-react-hw-04-movies/index.html"
+  },
+  {
+    "revision": "a99097cb33100bf537ea",
+    "url": "/goit-react-hw-04-movies/static/css/2.3d72dcb8.chunk.css"
+  },
+  {
+    "revision": "da3dbb11372333fcec53",
+    "url": "/goit-react-hw-04-movies/static/css/main.d4ad6503.chunk.css"
+  },
+  {
+    "revision": "a99097cb33100bf537ea",
+    "url": "/goit-react-hw-04-movies/static/js/2.12a37269.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-04-movies/static/js/2.12a37269.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da3dbb11372333fcec53",
+    "url": "/goit-react-hw-04-movies/static/js/main.e49a560f.chunk.js"
+  },
+  {
+    "revision": "8db16f721b0e583bda67",
+    "url": "/goit-react-hw-04-movies/static/js/runtime-main.677550f5.js"
+  }
+]);
